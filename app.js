5f5fcad552fffd0169f856e8b02d5d6084db409e
Miguel <miguelcaviedesm@gmail.com>
2019-12-08 12:18:04 -0400

    Agregado archivo data.json, por-hacer

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 
 const argv = require("./config/yargs").argv;
 
+const porHacer = require("./por-hacer/por-hacer");
+
 //console.log(argv);
 
 let comando = argv._[0];
 
 switch (comando) {
   case "crear":
-    console.log("Crear por hacer");
+    // console.log("Crear por hacer");
+    let tarea = porHacer.crear(argv.descripcion);
+    console.log(tarea);
     break;
 
   case "listar":
