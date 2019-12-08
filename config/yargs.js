e0b3619fa75e1c58af1c98c3eb071170f405c312
Miguel <miguelcaviedesm@gmail.com>
2019-12-08 16:30:18 -0400

    Aumento borrar

--- config/yargs.js
+++ config/yargs.js
@@ -1,22 +1,25 @@
+const descipcion = {
+  demand: true,
+  alias: "d",
+  desc: "Descripcion de la tarea"
+};
+
+const completado = {
+  demand: true,
+  alias: "c",
+  desc: "Marca como completado o pendiente la tarea"
+};
+
 const argv = require("yargs")
   .command("crear", "Crear un elemento por hacer", {
-    descripcion: {
-      demand: true,
-      alias: "d",
-      desc: "Descripcion de la tarea"
-    }
+    descripcion
   })
   .command("actualizar", "Actualizar el estado compretado de una tarea", {
-    descripcion: {
-      demand: true,
-      alias: "d",
-      desc: "Descripcion de la tarea"
-    },
-    completado: {
-      demand: true,
-      alias: "c",
-      desc: "Marca como completado o pendiente la tarea"
-    }
+    descripcion,
+    completado
+  })
+  .command("borrar", "Borrar una tarea", {
+    descripcion
   })
   .help().argv;
 
